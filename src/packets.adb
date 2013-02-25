6f9bce46a18b0a32cc68d18f88a8baa7a921607f
Daniel Rapp <danielrappt@gmail.com>
2013-02-25 14:24:37 +0100

    Added initial packet specification

---
+++ src/packets.adb
@@ -0,0 +1,19 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with TJa.Calendar; use TJa.Calendar;
+with TJa.Sockets; use TJa.Sockets;
+
+package Packets is
+  type Packet_Type is private;
+
+  function Assemble_Packet(Packet : Packet_Type)
+    return Unbounded_String;
+  function Disassemble_Packet(Raw_Packet : Unbounded_String)
+    return Packet_Type;
+private
+  type Packet_Type is record
+    Header : Character;
+    Message : Unbounded_String;
+  end record;
+
+  function HMS_Time return Unbounded_String;
+end Packets;
