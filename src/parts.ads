2e546658af5f375d5506c2c441d918db92094290
Daniel Rapp <danielrappt@gmail.com>
2013-02-25 18:24:11 +0100

    Added position and part type and renamed rotation_vector_type to to the more general vector_type

--- src/parts.ads
+++ src/parts.ads
@@ -1,12 +1,27 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package Parts is
   type Part_Type is private;
-  type Rotation_Vector_Type is private;
+  type Vector_Type is private;
 
-  function Create_Rotation_Vector(X, Y, Z : in Integer) return Rotation_Vector_Type;
-  procedure Rotate(Part : in out Part_Type; Rotation_Vector : in Rotation_Vector_Type);
+  function Get(Str : in Unbounded_String; ID : in Positive) return Part_Type;
+
+  function Create_Part(Rotation : in Vector_Type := (X => 0, Y => 0, Z => 0);
+                       Position : in Vector_Type := (X => 0, Y => 0, Z => 0))
+    return Part_Type;
+  function Create_Vector(X, Y, Z : in Integer := (X => 0, Y => 0, Z => 0))
+    return Vector_Type;
+
+  procedure Rotate(Part : in out Part_Type; Vector : in Vector_Type);
+  -- Also known as "Move"
+  procedure Traverse(Part : in out Part_Type; Vector : in Vector_Type);
 private
-  type Part_Type is null;
-  type Rotation_Vector_Type is record
+  type Part_Type is record
+    ID : Positive;
+    Rotation : Vector_Type;
+    Position : Vector_Type;
+  end record;
+  type Vector_Type is record
     X, Y, Z : Normal;
   end record;
 
