391e04a92ff3d29d642cbfd2a4e10708c3975054
Daniel Rapp <danielrappt@gmail.com>
2013-03-02 14:57:15 +0100

    Fixed indentation in misc

--- src/misc.adb
+++ src/misc.adb
@@ -5,16 +5,16 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with TJa.Sockets; use TJa.Sockets;
 
 package body Misc is
-	-- Get the whole line from a socket into an unbounded string
-	procedure Get_Line(Socket : in Socket_Type; Str : out Unbounded_String) is
-		Buffer_Size : constant := 2000;
-		Buffer : String(1..Buffer_Size);
-		Last : Positive;
-	begin
-		Str := To_Unbounded_String("");
-		while Length = Buffer_Size loop
-			Get_Line(Socket, Buffer, Last);
-			Append(Str, Buffer(1..Last));
-		end loop;
-	end Get_Line;
+  -- Get the whole line from a socket into an unbounded string
+  procedure Get_Line(Socket : in Socket_Type; Str : out Unbounded_String) is
+    Buffer_Size : constant := 2000;
+    Buffer : String(1..Buffer_Size);
+    Last : Positive;
+  begin
+    Str := To_Unbounded_String("");
+    while Length = Buffer_Size loop
+      Get_Line(Socket, Buffer, Last);
+      Append(Str, Buffer(1..Last));
+    end loop;
+  end Get_Line;
 end Misc;
