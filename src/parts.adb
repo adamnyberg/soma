1b47260dd8955ca821ee9026426b5cf5620c6ce7
Daniel Rapp <danielrappt@gmail.com>
2013-03-02 21:11:15 +0100

    Added initial parts implementation

---
+++ src/parts.adb
@@ -0,0 +1,26 @@
+-- adany869 Adam Nyberg, danth407 Daniel Rapp,
+-- harpe493 Harald Petterson, jonta760 Jonas Tarassu
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+package body Parts is
+  procedure Parse(Raw_Parts : in Unbounded_String, Part : out Part_Type) is
+  begin
+    null;
+  end Parse;
+
+  procedure Parse(Raw_Parts : in Unbounded_String, Parts : out Part_Type) is
+  begin
+    null;
+  end Parse;
+
+  procedure Rotate(Part : in out Part_Type; Vector : in Vector_Type) is
+  begin
+    null;
+  end Rotate;
+
+  procedure Traverse(Part : in out Part_Type; Vector : in Vector_Type) is
+  begin
+    null;
+  end Traverse;
+end Parts;
--- src/parts.ads
+++ src/parts.ads
@@ -6,8 +6,11 @@ with Figures; use Figures;
 
 package Parts is
   type Part_Type is private;
+  type Parts_Type is private;
   type Vector_Type is private;
 
+  procedure Parse(Raw_Part : in Unbounded_String, Part : out Part_Type);
+  procedure Parse(Raw_Parts : in Unbounded_String, Parts : out Parts_Type);
   procedure Rotate(Part : in out Part_Type; Vector : in Vector_Type);
   -- Also known as "Move"
   procedure Traverse(Part : in out Part_Type; Vector : in Vector_Type);
@@ -16,6 +19,7 @@ private
     Position : Vector_Type;
     Rotation : Vector_Type;
   end record;
+  type Parts_Type is array(range <>) of Part_Type;
 
   procedure Rotate_X(Part : in out Part_Type);
   procedure Rotate_Y(Part : in out Part_Type);
