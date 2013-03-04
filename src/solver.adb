3c08386e8bd9f9aced19ef2d48fc91a0edc5cb03
Daniel Rapp <danielrappt@gmail.com>
2013-03-04 16:25:18 +0100

    Added figure and parts parsing in solver.adb

--- src/solver.adb
+++ src/solver.adb
@@ -5,7 +5,8 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with TJa.Sockets; use TJa.Sockets;
 with Protocol; use Protocol;
 with Packets; use Packets;
-with Figures; use Figures;
+with Figures;
+with Parts;
 with Misc;
 
 with Ada.Text_IO;
@@ -14,6 +15,9 @@ procedure Solver is
   Socket : Socket_Type;
   Raw_Packet : Unbounded_String;
   Packet : Packet_Type;
+
+  New_Parts : Parts.Parts_Type(1..7);
+  Figure : Figures.Figure_Type(1);
 begin
   Initiate(Socket);
   Connect(Socket, "localhost", 3333);
@@ -26,6 +30,8 @@ begin
     case Packet.Header is
       when INITIATE_HEADER => Protocol.Initiate(Socket, Packet);
       when CONFIRM_HEADER => Protocol.Confirm(Socket, Packet);
+      when PARTS_HEADER => New_Parts := Parts.Parse(Packet.Message);
+      when FIGURE_HEADER => Figure := Figures.Parse(Packet.Message);
       when others => Ada.Text_IO.Put(Packet.Header); exit;
     end case;
   end loop;
