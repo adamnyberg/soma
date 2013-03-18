e7216657a17addb5b4424d87ebe9db1e4771415b
adamnyberg <adam.nyberg@gmail.com>
2013-03-18 22:22:30 +0100

    added packages

--- src/bits.ads
+++ src/bits.ads
@@ -2,12 +2,13 @@
 -- harpe493 Harald Petterson, jonta760 Jonas Tarassu
 
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO; use Ada.Text_IO;
+with Misc; use Misc;
 
 package Bits is
   type Unsigned_Type is private;
   type Bits_Type is array (Natural range <>) of Unsigned_Type;
   BITS_LENGTH : constant Positive := 32;
-
 
   function Parse(Str : Unbounded_String) return Bits_Type;
 
@@ -15,4 +16,4 @@ package Bits is
 private
   type Unsigned_Type is mod 2**BITS_LENGTH;
 
-  end Bits;
+end Bits;
