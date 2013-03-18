0bdb7573809681d48f7bbbb3721be8d71cbe8e75
Daniel Rapp <danielrappt@gmail.com>
2013-03-18 11:38:20 +0100

    Added bits test

---
+++ tests/test_bits.adb
@@ -0,0 +1,14 @@
+-- adany869 Adam Nyberg, danth407 Daniel Rapp,
+-- harpe493 Harald Petterson, jonta760 Jonas Tarassu
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Tests; use Tests;
+with Bits; use Bits;
+
+procedure Test_Bits is
+  Test_Bits_Parse : Unbounded_String := To_Unbounded_String("10011");
+  Bits : Bits_Type(1..1);
+begin
+  Bits_Seq := Bits.Parse( Test_Bits_Parse );
+  Test( To_String(Bits_Seq), Test_Bits_Parse );
+end Test_Bits;
