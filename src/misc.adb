b48f625d1c448aac5f66fa2a20a877fa0dc97857
adamnyberg <adam.nyberg@gmail.com>
2013-03-18 21:20:42 +0100

    added body for integer divide

--- src/misc.adb
+++ src/misc.adb
@@ -1,9 +1,20 @@
 -- adany869 Adam Nyberg, danth407 Daniel Rapp,
 -- harpe493 Harald Petterson, jonta760 Jonas Tarassu
 
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 package body Misc is
+  -- Get the whole line from a socket into an unbounded string
+  procedure Get_Line(Socket : in Socket_Type; Str : out Unbounded_String) is
+    Buffer_Size : constant := 2000;
+    Buffer : String(1..Buffer_Size);
+    Last : Positive := Buffer_Size;
+  begin
+    Str := To_Unbounded_String("");
+    while Last = Buffer_Size loop
+      Get_Line(Socket, Buffer, Last);
+      Append(Source => Str, New_Item => Buffer(1..Last));
+    end loop;
+  end Get_Line;
+
   function Unbounded_Slice(
       Source : in Unbounded_String;
       Low : in Positive;
@@ -19,12 +30,12 @@ package body Misc is
     Pattern : in String;
     Start : out Unbounded_String;
     Rest : out Unbounded_String;
-    Skip : in Natural := 0) is
+    Skip : in Positive := 1) is
 
     Tmp_Str : Unbounded_String;
     Space_Index : Natural := 0;
   begin
-    for I in 0..Skip loop
+    for I in 1..Skip loop
       Tmp_Str := Unbounded_Slice(Str, Space_Index+1, Length(Str));
       Space_Index := Space_Index + Index(Tmp_Str, Pattern);
     end loop;
@@ -32,4 +43,9 @@ package body Misc is
     Start := Unbounded_Slice( Str, 1, Space_Index-1 );
     Rest := Unbounded_Slice( Str, Space_Index+1, Length(Str) );
   end Split;
+
+  function Divide_With_Ceil(Int1, Int2 : Integer) return Integer is
+  begin
+    return Integer(Float'Ceiling(Float(Int1) / Float(Int2)));
+  end Divide_With_Ceil;
 end Misc;
