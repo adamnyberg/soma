d8cbb4e0081d5d3695cbc954f57a94be0b82d99c
adamnyberg <adam.nyberg@gmail.com>
2013-03-18 22:13:37 +0100

    removed Get_Line() from Misc

--- src/misc.adb
+++ src/misc.adb
@@ -2,18 +2,6 @@
 -- harpe493 Harald Petterson, jonta760 Jonas Tarassu
 
 package body Misc is
-  -- Get the whole line from a socket into an unbounded string
-  procedure Get_Line(Socket : in Socket_Type; Str : out Unbounded_String) is
-    Buffer_Size : constant := 2000;
-    Buffer : String(1..Buffer_Size);
-    Last : Positive := Buffer_Size;
-  begin
-    Str := To_Unbounded_String("");
-    while Last = Buffer_Size loop
-      Get_Line(Socket, Buffer, Last);
-      Append(Source => Str, New_Item => Buffer(1..Last));
-    end loop;
-  end Get_Line;
 
   function Unbounded_Slice(
       Source : in Unbounded_String;
