5dd06158b21141e21d37a9a8bd627e60900c083f
Daniel Rapp <danielrappt@gmail.com>
2013-03-19 09:37:05 +0100

    Changed bits type

--- src/bits.ads
+++ src/bits.ads
@@ -5,7 +5,11 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Bits is
   type Unsigned_Type is private;
-  type Bits_Type is array (Natural range <>) of Unsigned_Type;
+  type Bits_Raw_Type is array (Natural range <>) of Unsigned_Type;
+  type Bits_Type(Num_Bits_Raw_Type : Natural) is record
+    Bits : Bits_Raw_Type(1..Num_Bits_Raw_Type);
+    Length : Natural;
+  end record;
   BITS_LENGTH : constant Positive := 32;
 
   function Parse(Str : Unbounded_String) return Bits_Type;
