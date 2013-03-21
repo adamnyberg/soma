854c256bd837307b7d91d2fc618238f1f63ed5fb
Daniel Rapp <danielrappt@gmail.com>
2013-03-21 13:12:24 +0100

    Added solver pseudocode

--- src/solver.adb
+++ src/solver.adb
@@ -2,8 +2,24 @@
 -- harpe493 Harald Petterson, jonta760 Jonas Tarassu
 
 with Parts; use Parts;
+with Figure; use Figure;
 
 package body Solver is
-
-
+  procedure Solve(Parts : Parts_Type; Figure : Figure_Type) is
+  begin
+    -- l = a list with unused parts
+    -- do while l is nonempty
+    --   find hole to place part
+    --   if 
+    --     try next piece
+    --   else
+    --     try
+    --
+    --
+    --
+    -- find position to place part,
+    -- if no overlap,
+    --   try the next piece
+    --
+  end Solve;
 end Solver;
