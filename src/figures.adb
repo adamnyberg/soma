fd91136749b690a980977af42ec721db1fd98579
Harald Pettersson <harpe493@student.liu.se>
2013-03-21 09:16:24 +0100

    Fixed issue with discriminant in Figures

--- src/figures.adb
+++ src/figures.adb
@@ -1,6 +1,5 @@
 -- adany869 Adam Nyberg, danth407 Daniel Rapp,
 -- harpe493 Harald Petterson, jonta760 Jonas Tarassu
-
 package body Figures is
   function Parse(Raw_Figure : in Unbounded_String) return Figure_Type is
     Figure_ID : Unbounded_String;
@@ -11,13 +10,12 @@ package body Figures is
   begin
     Misc.Split( Raw_Figure, " ", Figure_ID, Rest );
     Misc.Split( Rest, " ", Dimension, Bits_Str );
-    Figure_Size := Misc.Divide_With_Ceil(Length(Rest), Bits.BITS_LENGTH);
+    Figure_Size := Misc.Divide_With_Ceil(Length(Bits_Str), Bits.BITS_LENGTH);
     declare
       Figure : Figure_Type(Figure_Size);
     begin
       Figure.ID := Figure_ID; 
       Figure.Dimension := Vector.Parse( Dimension );
-      Put(To_String(Bits_Str));
       Figure.Structure := Bits.Parse( Bits_Str );
       return Figure;
     end;
