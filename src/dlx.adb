8e84293d4378a5c3ef83b38177968215c2c24660
Harald Pettersson <harpe493@student.liu.se>
2013-03-22 11:48:54 +0100

    More DLX solving functions

--- src/dlx.adb
+++ src/dlx.adb
@@ -1,6 +1,6 @@
 
 
-procedure DLX is
+package DLX is
   procedure Delete_Node(Node : in Linked_Matrix) is
   begin
     Node.Left.Right := Node.Right;
@@ -23,17 +23,19 @@ procedure DLX is
     loop
       Delete_Node(Temp);
       Temp := Temp.Right;
-      exit when Temp = Node;
+      exit when Temp.Right = Temp;
+      Delete_Node(Temp);
     end loop;
   end Delete_Row;
 
   procedure Reset_Row(Node : in Linked_Matrix) is
-    Temp : Linked_Matrix := Node;
+    Temp : Linked_Matrix := Node.Left;
   begin
     loop
       Reset_Node(Temp);
-      Temp := Temp.Right;
+      Temp := Temp.Left;
       exit when Temp = Node;
+      Reset_Node(Node);
     end loop;
   end Reset_Row;
 
@@ -43,23 +45,40 @@ procedure DLX is
     loop
       Delete_Node(Temp);
       Temp := Temp.Up;
-      exit when Temp = Node;
+      exit when Temp.Up = Temp;
+      Delete_Node(Temp);
     end loop;
   end Delete_Column;
 
   procedure Reset_Column(Node : in Linked_Matrix) is
-    Temp : Linked_Matrix;
+    Temp : Linked_Matrixi := Node.Down;;
   begin
     loop
       Reset_Node(Temp);
-      Temp := Temp.Up;
+      Temp := Temp.Down;
       exit when Temp = Node;
+      Reset_Node(Node);
     end loop;
   end Reset_Column;
 
   procedure Delete_DLX(Node : in Linked_Matrix) is
     Temp : Linked_Matrix := Node.Up;
   begin
+    Delete(Node);
+    loop
+      if Temp.IS_HEADER = FALSE then --HÄR
+	Delete_Row(Temp);
+      else
+	Delete_Node(Temp);
+      end if;
+      exit when Temp.Up = Temp;
+    end loop;
+
+    if Node.Right /= Node then
+      Delete_DLX(Node.Right);
+    end if;
+
+    if False then
     Delete_Node(Node);
     Delete_Node(Temp);
     while Temp.Up /= Temp loop
@@ -82,36 +101,76 @@ procedure DLX is
     --  Temp := Temp.Right;
     --  exit when Temp := Node;
     --end loop;
+    end if;
   end Delete_DLX;
 
   procedure Reset_DLX(Node : in Linked_Matrix) is
     Temp : Linked_Matrix := Node.Up;
   begin
-    Delete_Node(Node);
-    Delete_Node(Temp);
-    while Temp.Up /= Temp loop
-      Delete_Row(Temp.Up);
+    Reset(Node);
+    loop
+      Reset_Row(Temp);
+      Temp := Temp.Up;
+      exit when Temp = Node;
     end loop;
 
-    --loop
-    --  Delete_Row(Temp);
-    --  Temp := Temp.Up;
-    --  exit when Temp = Node;
-    --end loop;
-
-    Temp := Node.Right;
-    Delete_Node(Temp);
-    while Temp.Right /= Temp loop
-      Delete_Column(Temp.Right);
-    end loop;
-    --loop
-    --  Delete_Column(Temp);
-    --  Temp := Temp.Right;
-    --  exit when Temp := Node;
-    --end loop;
+    if Node.Right /= Node then
+      Reset_DLX(Node.Right);
+    end if;
   end Reset_DLX;
 
-  Head : Linked_Matrix;
-begin
- Null;
+  function Count_Ones(Col_Header : in Linked_Matrix) return Integer is
+    Temp : Linked_Matrix := Col_Header.Down;
+    Ones : Integer := 0;
+  begin
+    while Temp /= Col_Header loop
+      Ones := Ones + 1;
+      Temp := Temp.Up;
+    end loop;
+    return Ones;
+  end Count_Ones;
+
+  function Fewest_Ones_2(Col_Header1, Col_Header2 : in Linked_Matrix) return Linked_Matrix is
+  begin
+    if Count_Ones(Col_Header1) > Count_Ones(Col_Header2) then
+      return Col_Header1;
+    else
+      return Col_Header2;
+    end if;
+  end Fewest_Ones;
+
+  function Fewest_Ones_All(Header : in Linked_Matrix) return Linked_Matrix is
+    Column : Linked_Matrix := Header.Right;
+    Temp : Linked_Matrix := Column.Right;
+  begin
+    while Temp /= Header loop
+      Column := Fewest_Ones_2(Column,Temp);
+      Temp := Temp.Right;
+    end loop;
+  end Fewest_Ones_All;
+  
+  procedure Solve_DLX(Header : in Linked_Matrix;
+    Selected : in out Linked_Resulting_List_Pointer; Solved : out Boolean) is
+
+    Head : Linked_Matrix;
+    Selected_Col : Linked_Matrix;
+    Selected_Row : Linked_Matrix;
+    Selected_Node : Linked_Matrix;
+    Resulting_Matrices : Linked_Resulting_List_Pointer;
+  begin
+    if Is_Empty(Header) then
+      --Solution Found! Return Resulting_Matrices
+      return True;
+    elsif Count_Ones(Fewest_Ones_All(Header)) = 0 then
+      --GO BACK!/Give up;
+      Reset_DLX(Last(Resulting_Matrices).Row);
+      Remove_Last(Resulting_Matrices);
+    else
+      Selected_Col := Fewest_Ones_All(Header);
+      Selected_Node := Selected_Col.Down;
+      Resulting_Matrices.Right := Get_Row_Header_Info(Selected_Node);
+      Delete_DLX(Selected_Node);
+      Solve_DLX(Header, Resulting_Matrices);
+    end if;
+  end Solve_DLX;
 end DLX;
