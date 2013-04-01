3eb26abe116571c47a4549f4aebbfb55e1a61712
Harald Pettersson <harpe493@student.liu.se>
2013-04-01 16:40:08 +0200

    Worked on DLX.

--- src/dlx.adb
+++ src/dlx.adb
@@ -1,6 +1,52 @@
 
 
 package DLX is
+  procedure Put_Row(Row : in Linked_Matrix; Amount : Integer) is
+    Curr : Linked_Matrix := Row.Right;
+    Prev : Linked_Matrix := Row;
+  begin
+    Put(Row.Data.Row & "  ");
+    loop
+      if Curr.Data.Column = 0 then
+	for I in Prev.Data.Column + 1..Amount loop
+	  Put(0);
+	  Put("  ");
+	end loop;
+	exit;
+      end if;
+      for I in Prev.Data.Column + 1..Curr.Data.Column loop
+	Put(0);
+	Put("  ");
+      end loop;
+      Put(1);
+      Prev := Curr;
+      Curr := Curr.Right;
+    end loop;
+  end Put_Row;
+
+  procedure Put(Header : in Linked_Matrix_Pointer) is
+    Temp : Linked_Matrix := Header.Cols;
+    Num_Of_Cols : Integer := 0;
+  begin
+    if Is_Empty(Header) then
+      Put("The Matrix is empty!");
+      return;
+    end if;
+    loop
+      Put(Temp.Data.Cols & "  ");
+      Temp := Temp.Right;
+      Num_Of_Cols := Num_Of_Cols + 1;
+      exit when Temp.Data.Column = 1;
+    end loop;
+    Temp := Header.Rows;
+    loop
+      New_Line(2);
+      Put_Row(Temp, Num_Of_Cols);
+      Temp := Temp.Down;
+      exit when Temp.Data.Rows = 1;
+    end loop;
+  end Put;
+
   procedure Delete_Node(Node : in Linked_Matrix) is
   begin
     Node.Left.Right := Node.Right;
@@ -66,7 +112,7 @@ package DLX is
   begin
     Delete(Node);
     loop
-      if Temp.IS_HEADER = FALSE then --HÄR
+      if Temp.Data.Rows /= 0 then --HÄR
 	Delete_Row(Temp);
       else
 	Delete_Node(Temp);
@@ -123,9 +169,10 @@ package DLX is
     Temp : Linked_Matrix := Col_Header.Down;
     Ones : Integer := 0;
   begin
-    while Temp /= Col_Header loop
+    loop
       Ones := Ones + 1;
-      Temp := Temp.Up;
+      Temp := Temp.Down;
+      exit when Temp.Data.Rows = 0;
     end loop;
     return Ones;
   end Count_Ones;
@@ -139,38 +186,126 @@ package DLX is
     end if;
   end Fewest_Ones;
 
-  function Fewest_Ones_All(Header : in Linked_Matrix) return Linked_Matrix is
-    Column : Linked_Matrix := Header.Right;
+  function Fewest_Ones_All(Header : in Linked_Matrix_Pointer) return Linked_Matrix is
+    Column : Linked_Matrix := Header.Cols;
     Temp : Linked_Matrix := Column.Right;
   begin
-    while Temp /= Header loop
+    loop
       Column := Fewest_Ones_2(Column,Temp);
       Temp := Temp.Right;
+      exit when Temp.Data.Rows = 1;
     end loop;
+    return Column;
   end Fewest_Ones_All;
-  
-  procedure Solve_DLX(Header : in Linked_Matrix;
-    Selected : in out Linked_Resulting_List_Pointer; Solved : out Boolean) is
 
-    Head : Linked_Matrix;
-    Selected_Col : Linked_Matrix;
-    Selected_Row : Linked_Matrix;
-    Selected_Node : Linked_Matrix;
-    Resulting_Matrices : Linked_Resulting_List_Pointer;
+  function Has_A_Column_Without_Ones(Header : in Linked_Matrix_Pointer) return Boolean is
+    Temp : Linked_Matrix := Header.Cols;
   begin
-    if Is_Empty(Header) then
-      --Solution Found! Return Resulting_Matrices
+    loop
+      if Temp.Down.Data.Row = 0 then
+	return True;
+      end if;
+      Temp := Temp.Right;
+      exit when Temp.Data.Cols = 1;
+    end loop;
+  end Has_A_Column_Without_Ones;
+
+  function Is_Empty(Header : in Linked_Matrix_Pointer) return Boolean is
+    if Header.Right:= Null then
       return True;
-    elsif Count_Ones(Fewest_Ones_All(Header)) = 0 then
-      --GO BACK!/Give up;
-      Reset_DLX(Last(Resulting_Matrices).Row);
-      Remove_Last(Resulting_Matrices);
     else
-      Selected_Col := Fewest_Ones_All(Header);
-      Selected_Node := Selected_Col.Down;
-      Resulting_Matrices.Right := Get_Row_Header_Info(Selected_Node);
-      Delete_DLX(Selected_Node);
-      Solve_DLX(Header, Resulting_Matrices);
+      return False;
     end if;
+  end Is_Empty;
+
+  function Last(Resulting_List : in Linked_Resulting_List_Pointer) 
+		  return Linked_Resulting_List_Pointer is
+    Curr, Temp : Linked_Resulting_List_Pointer := Resulting_List;
+  begin
+    if Curr.next := Null then
+      return Curr;
+    else
+      Curr := Curr.Next;
+    end if;
+  end Last;
+
+  function Get_Row_Header_Info(Node : in Linked_Matrix) return Linked_Resulting_List is
+    Curr : Linked_Resulting_List;
+    Temp : Linked_Matrix := Node;
+  begin
+    while Temp.Data.Cols /= 0 loop
+      Temp := Temp.Left;
+    end loop;
+
+    Curr.Row := Temp;
+    Curr.Part := Temp.Part;
+    Curr.Position := Temp.Position;
+    Curr.Rotated := Temp.Rotated;
+    Curr.Next := Null;
+    return Curr;
+  end Get_Row_Header_Info;
+
+  function Choose_Next_Row(Previous : in Linked_Matrix) return Linked_Matrix is
+  begin
+    return Previous.Down;
+  end Choose_Next_Row;
+
+  function Choose_Next_One(Header : in Linked_Matrix_Pointer; Previous : in Linked_Matrix)
+	return Linked_Matrix is
+    Node : Linked_Matrix;
+  begin
+    if Previous.Down.IS_HEADER then
+      Node := Previous.Down.Right.Down;
+    else
+      Node := Previous.Down;
+    end if;
+
+    if Node.Left.Data.Cols = 0 then--ROW_HEADER then
+      return Previous.Down.Right.Down;
+    else
+      return Choose_Next_One(Node);
+    end if;
+  end Choose_Next_One;
+
+  procedure Free is
+    new Ada.Unchecked_Deallocation(Linked_Matrix, Linked_Matrix_Pointer);
+
+  procedure Remove_Last(Linked_List : in out Linked_Resulting_List_Pointer) is
+    Semi_Last, Garbage : Linked_Matrix_Pointer;
+  begin
+    Semi_Last := Linked_List;
+    while Semi_Last.Next.Next /= Null loop
+      Semi_Last := Semi_Last.Next;
+    end loop;
+    Garbage := Semi_Last;
+    Semi_Last.Next := Null;
+    Free(Garbage);
+  end Remove_Last;
+
+  procedure Solve_DLX(Header : in Linked_Matrix_Pointer;
+    Selected : in out Linked_Resulting_List_Pointer := Null; Solved : out Boolean) is
+    Selected_Row : Linked_Matrix := Header.Rows.Up;;
+    Selected_Node : Linked_Matrix;
+    Solution : Linked_Resulting_List_Pointer;
+  begin
+    while not Solved loop
+      if Is_Empty(Header) then
+	--Solution Found! Return Resulting_Matrices
+	Solved := True;
+      elsif Has_Only_Zeros_Column(Header)
+	or else Selected_Row.Data.Row = Header.Rows.Data.Row then
+	--GO BACK!/Give up (When there is atleast one row with no ones or
+	--all rows have been selected once.
+	Reset_DLX(Last(Solution).Row);
+	Remove_Last(Solution);
+	exit;
+      else
+	Selected_Row := Choose_Next_Row(Selected_Row);
+	Selected_Node := Selected_Row.Right;
+	Last(Solution).Next := new Get_Row_Header_Info'(Selected_Node);
+	Delete_DLX(Selected_Node);
+	Solve_DLX(Header, Solution, Solved);
+      end if;
+    end loop;
   end Solve_DLX;
 end DLX;
--- src/dlx.ads
+++ src/dlx.ads
@@ -1,7 +1,18 @@
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 package DLX is
   type Linked_Resulting_List is private;
   type Linked_Resulting_List_Pointer is access Linked_Resulting_List;
+  type Linked_Resulting_List is
+    record
+      Row : Linked_Matrix;
+      Part : Integer;
+      Position, Rotated : Vector;
+      Next : Linked_Resulting_List_Pointer;
+    end record;
+  function Is_Empty(Header : in Linked_Matrix_Pointer) return Boolean;
+  procedure Put_Row(Row : in Linked_Matrix; Amount : Integer);
+  procedure Put(Header : in Linked_Matrix_Pointer);
   procedure Delete_Node(Node : in Linked_Matrix);
   procedure Reset_Node(Node : in Linked_Matrix);
   procedure Delete_Row(Node : in Linked_Matrix);
@@ -10,15 +21,15 @@ package DLX is
   procedure Reset_Column(Node : in Linked_Matrix);
   procedure Delete_DLX(Node : in Linked_Matrix);
   procedure Reset_DLX(Node : in Linked_Matrix);
-  procedure Solve_DLX(Header : in Linked_Matrix;
+  function Count_Ones(Col_Header : in Linked_Matrix) return Integer;
+  function Fewest_Ones_2(Col_Header1, Col_Header2 : in Linked_Matrix) return Linked_Matrix;
+  function Fewest_Ones_All(Header : in Linked_Matrix_Pointer) return Linked_Matrix;
+  function Has_A_Column_Without_Ones(Header: in Linked_Matrix_Pointer) return Boolean;
+  function Last(Resulting_List : in Linked_Resulting_List_Pointer)
+		  return Linked_Resulting_List_Pointer;
+  function Get_Row_Header_Info(Node : in Linked_Matrix) return Linked_Resulting_List;
+  function Choose_Next_One(Header : in Linked_Matrix_Pointer; Previous : in Linked_Matrix)
+	return Linked_Matrix;
+  procedure Solve_DLX(Header : in Linked_Matrix_Pointer;
     Selected : in out Linked_Resulting_List_Pointer; Solved : out Boolean);
-private
-  type Linked_Resulting_List is
-    record
-      Row : Linked_Matrix;
-      Part : Integer;
-      Traversed, Rotated : Vector;
-      Next : Linked_Resulting_List_Pointer;
-    end record;
 end DLX;
-
